d6bf4274d50470e92add20524aa237d651fd741c
ABs <nccuong317qb@gmail.com>
2023-12-14 21:23:26 +0700

    file submission is res.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "main.h"
-#include "restaurant.cpp"
+#include "res.cpp"
 #include <chrono>
 
 int main(int argc, char* argv[]) {
-    string fileName = "test.txt";
+    string fileName = "output_random.txt";
     auto start = std::chrono::high_resolution_clock::now();
     simulate(fileName);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
+    //cin.get();
     std::cout << "Time run: " << duration.count() << "(s)\n";
     system("pause");
     return 0;
---
+++ randtc.py
@@ -0,0 +1,41 @@
+import random
+import string
+
+# Tạo danh sách các loại chuỗi
+categories = ["LAPSE", "HAND", "KEITEIKEN", "KOKUSEN", "LIMITLESS", "CLEAVE"]
+
+# Tạo chuỗi ngẫu nhiên với độ dài 30 kí tự
+def generate_random_string():
+    return ''.join(random.choices(string.ascii_letters, k= random.randint(1, 200) ))
+
+# Số lượng chuỗi cần tạo
+num_strings = 100000
+
+# Xử lý và ghi vào file
+with open("output_random.txt", "w") as file:
+    print("Write file in output_random.txt")
+    file.write(f"MAXSIZE {random.randint(1, 10)}\n")
+    last_category = None
+    for _ in range(num_strings):
+        available_categories = [c for c in categories if c != last_category]
+        category = random.choice(available_categories)
+        if category == "HAND":
+            #file.write(f"{category}\n")
+            continue
+        elif category == "KEITEIKEN":
+            file.write(f"{category} {random.randint(1, 10)}\n")
+        elif category == "KOKUSEN":
+            file.write(f"{category}\n")
+        elif category == "LIMITLESS":
+            file.write(f"{category} {random.randint(1, 10)}\n")
+        elif category == "CLEAVE":
+            file.write(f"{category} {random.randint(1, 10)}\n")
+        else:
+            file.write(f"{category} {generate_random_string()}\n")
+        
+        last_category = category
+    
+    file.flush()  # Đảm bảo rằng tất cả dữ liệu đã được đẩy vào file
+    file.close()  # Đóng file
+
+print("Process finished !")
